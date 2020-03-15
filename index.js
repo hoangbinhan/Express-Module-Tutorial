de524b584808b581e97126d7ad0f11f2fd33a750
Hoang An <you@example.com>
2020-03-15 14:43:38 +0700

    lession-1-helloWorld

---
+++ index.js
@@ -0,0 +1,5 @@
+var express = require('express');
+app = express();
+const port = 3000;
+app.get('/',(req,res)=> res.send('hello world'));
+app.listen(port,()=> console.log(`Example app listening on port ${port}!`));
